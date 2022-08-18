d3338d4f15fa238492b5600c9cf2fa4fed342f95
jahan rayane <idhamdammahom@gmail.com>
2022-08-18 09:28:49 +0430

    Api-key

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
 ##CREATE DB
